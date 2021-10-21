ccad718d3a88f012b306752ad2095aaf81f23e4c
NhanSimple <91020869+NhanSimple@users.noreply.github.com>
2021-10-21 15:20:28 +0700

    Add files via upload

---
+++ chitiet.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+  
+</head>
+<body>
+<h1>Chi Tiết Thuốc</h1>
+<div class="container mt-3">
+ 
+  <table class="table">
+    <thead>
+      <tr>
+      <th>id</th>
+        <th>Tên thuốc</th>
+        <th>Loại thuốc</th>
+        <th>Mã vạch</th>
+        <th>Liều lượng</th>
+        <th>Mã</th>
+        <th>Giá nhập</th>
+        <th>Giá bán</th>
+        <th>Trạng thái Hạn sử dụng</th>
+        <th>Công ty</th>
+        <th>Ngày sản xuất</th>
+        <th>Ngày hết hạn</th>
+        <th>Nơi sản xuất</th>
+        <th>Số lượng</th>
+        <button type="button" class="btn">Thêm</button>
+  <button type="button" class="btn btn-primary">Sửa</button>
+  <button type="button" class="btn btn-secondary">Xóa</button>
+      </tr>
+    </thead>
+   
+  </table>
+</div>
+
+</body>
+</html>
---
+++ dsthuoc.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<h1>Danh Sách Thuốc</h1>
+<div class="container mt-3">
+ 
+  <table class="table">
+    <thead>
+      <tr>
+      <th>id</th>
+        <th>Tên thuốc</th>
+        <th>Loại thuốc</th>
+        <th>Mã vạch</th>
+        <th>Liều lượng</th>
+        <th>Mã</th>
+        <th>Giá nhập</th>
+        <th>Giá bán</th>
+        <th>Trạng thái Hạn sử dụng</th>
+        <th>Công ty</th>
+        <th>Ngày sản xuất</th>
+        <th>Ngày hết hạn</th>
+        <th>Nơi sản xuất</th>
+        <th>Số lượng</th>
+        <th><a href="chitiet.php">Chi tiết</a></th>
+      </tr>
+    </thead>
+   
+  </table>
+</div>
+
+</body>
+</html>
